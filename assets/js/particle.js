04ad8d647ccdc7d4c7a878226dae86dd37400179
keslymartinez <keslymartinezvaldes@gmail.com>
2018-02-12 10:13:08 -0300

    agregando fondo de particulas

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -56,7 +56,7 @@ var pJS = function(tag_id, params){
       },
       size: {
         value: 20,
-        random: false,
+        random: true,
         anim: {
           enable: false,
           speed: 20,
