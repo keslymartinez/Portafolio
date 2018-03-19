31eabad730f4888542ef71562dacd20d7a698929
keslymartinez <keslymartinezvaldes@gmail.com>
2018-03-19 12:56:14 -0300

    actualizando portafolio

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,9 +1,15 @@
 
 
+
+
 var  typed = new Typed ('.black-text', {
 	strings:['Me encanta buscar cosas nuevas apasionada por la tecnologia estoy en constante aprendizaje que me lleve mas lejos.','Hola soy Kesly Martinez!','Frontend Developer!'],
 	typeSpeed:100,
 	backSpeed:100,
 	fadeOut:true,
 	loop:true,
+})
+//boton para ver portafolio
+$('.btn').click(function() {
+  $('#skills').addClass('animated fadeInDown');
 })
