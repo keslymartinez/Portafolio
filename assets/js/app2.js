989613a716b9edded8095f9e7b8c54e110e10721
keslymartinez <keslymartinezvaldes@gmail.com>
2018-03-20 04:32:03 -0300

    subiendo collapsible

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,7 +1,6 @@
 
 
 
-
 var  typed = new Typed ('.white-text', {
 	strings:['Me encanta buscar cosas nuevas apasionada por la tecnologia estoy en constante aprendizaje que me lleve mas lejos.','Hola soy Kesly Martinez!','Frontend Developer!'],
 	typeSpeed:100,
@@ -10,6 +9,11 @@ var  typed = new Typed ('.white-text', {
 	loop:true,
 })
 //boton para ver portafolio
-$('.btn').click(function() {
-  $('#skills').addClass('animated fadeInDown');
-})
+
+ 
+     
+ var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+       
